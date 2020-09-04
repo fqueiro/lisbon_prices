9f03efb9984788289582656929819918f8e16e2c
fqueiro <66741500+fqueiro@users.noreply.github.com>
2020-09-04 20:33:47 +0100

    add more locations

--- lisbon_prices.py
+++ lisbon_prices.py
@@ -2,17 +2,7 @@ from urllib.request import Request, urlopen
 import re
 from csv import writer
 from datetime import date
-
-x='idea'
-
-req = Request('https://www.'+x+'lista.pt/comprar-casas/lisboa/', headers={'User-Agent': 'Mozilla/5.0'})
-f = urlopen(req).read().decode("utf-8")
-price=re.findall('Preço médio nesta zona \d*[.]\d* eur',f)
-print(price)
-price=re.findall('\d*[.]\d*',price[0])
-today = date.today()
-d1 = today.strftime("%d/%m/%Y")
-price.append(d1)
+import time
 
 def append_list_as_row(file_name, list_of_elem):
     # Open file in append mode
@@ -21,5 +11,21 @@ def append_list_as_row(file_name, list_of_elem):
         csv_writer = writer(write_obj)
         # Add contents of list as last row in the csv file
         csv_writer.writerow(list_of_elem)
-        
-append_list_as_row('lisbon_prices.csv',price)
+
+x='idea'
+base_url = 'https://www.'+x+'lista.pt/comprar-casas/lisboa/'
+areas = ['','belem/','ajuda/','alcantara/','estrela/']
+#areas = ['','belem/','ajuda/','alcantara/','estrela/','campo-de-ourique/','misericordia/','santo-antonio/','santa-maria-maior/']
+
+today = date.today()
+d1 = today.strftime("%d/%m/%Y")
+for area in areas:
+    req = Request(base_url+area, headers={'User-Agent': 'Mozilla/5.0'})
+    f = urlopen(req).read().decode("utf-8")
+    price=re.findall('Preço médio nesta zona \d*[.]\d* eur',f)
+    price=re.findall('\d*[.]\d*',price[0])
+    price.append(area)
+    price.append(d1)
+    append_list_as_row('lisbon_prices2.csv',price)
+    time.sleep(60)
+
